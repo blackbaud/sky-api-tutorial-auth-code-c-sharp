59fa322e8ea0bb95c416d5ab0166c02261caab57
stevebrush <steve.brush@blackbaud.com>
2016-05-17 16:46:39 -0400

    Initial commit.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace SampleApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(IRouteBuilder routes)
+        {
+            routes.MapRoute(
+                name: "Default",
+                template: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" }
+            );   
+            routes.MapRoute(
+                name: "Authorization",
+                template: "auth/{action}",
+                defaults: new { action = "authorized" }
+            );
+        }
+    }
+}
---
+++ App_Start/Startup.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        
+        private static RequestDelegate ChangeContextToHttps(RequestDelegate next)
+        {
+            return async context =>
+            {
+                context.Request.Scheme = "https";
+                await next(context);
+            };
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.Use(ChangeContextToHttps);
+            app.UseMvc(RouteConfig.RegisterRoutes);
+        }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.ThreadCount = 4;
+                    options.NoDelay = true;
+                    options.UseHttps("testCert.pfx", "testPassword");
+                    options.UseConnectionLogging();
+                })
+                .UseUrls("https://*:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
