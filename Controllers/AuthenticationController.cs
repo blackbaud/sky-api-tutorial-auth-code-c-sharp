c8326e98404b305a80393a35f2cbd9b046d2784f
stevebrush <steve.brush@blackbaud.com>
2016-05-18 11:52:02 -0400

    Added login class.

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blackbaud.AuthCodeFlowTutorial.Controllers {
+    public class AuthenticationController : Controller {
+        
+        [HttpGet("~/auth/authenticated")]
+        public ActionResult Authenticated() {
+            return Json(new { 
+                authenticated = false
+            });
+        }
+        
+        [HttpGet("~/auth/login")]
+        public ActionResult LogIn() {
+            // Instruct the OIDC client middleware to redirect the user agent to the identity provider.
+            // Note: the authenticationType parameter must match the value configured in Startup.cs
+            return Challenge(new AuthenticationProperties { 
+                RedirectUri = "https://localhost:5000/"
+            }, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("~/signout"), HttpPost("~/signout")]
+        public ActionResult SignOut() {
+            // Instruct the cookies middleware to delete the local cookie created when the user agent
+            // is redirected from the identity provider after a successful authorization flow and
+            // to redirect the user agent to the identity provider to sign out.
+            return SignOut("ClientCookie", OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
