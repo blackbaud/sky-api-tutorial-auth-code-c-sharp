2807883fa2363b815d45f1ab8f6baa64348d8404
stevebrush <steve.brush@blackbaud.com>
2016-05-18 17:42:08 -0400

    Added check for access token.

--- App_Start/Startup.cs
+++ App_Start/Startup.cs
@@ -1,14 +1,9 @@
-using System;
 using System.IO;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Blackbaud.AuthCodeFlowTutorial
 {
@@ -17,11 +12,6 @@ namespace Blackbaud.AuthCodeFlowTutorial
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<SharedAuthenticationOptions>(options => {
-                options.SignInScheme = "ClientCookie";
-            });
-
-            services.AddAuthentication();
             services.AddMvc();
         }
         
@@ -41,32 +31,6 @@ namespace Blackbaud.AuthCodeFlowTutorial
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.Use(ChangeContextToHttps);
-            
-            var options = new OpenIdConnectOptions {
-                RequireHttpsMetadata = true,
-                SaveTokens = true,
-
-                // Note: these settings must match the application details
-                // inserted in the database at the server level.
-                ClientId = "5079f009-4b13-4699-945c-3b584b265694",
-                ClientSecret = "pPS8SS0dliufKJISqlgh3Cu7QTUCqdJfVUK95qiIZiU=",
-                PostLogoutRedirectUri = "https://localhost:5000",
-                CallbackPath = "/auth/callback",
-                
-                // Use the authorization code flow.
-                ResponseType = OpenIdConnectResponseTypes.Code,
-                ResponseMode = "query",
-
-                // Note: setting the Authority allows the OIDC client middleware to automatically
-                // retrieve the identity provider's configuration and spare you from setting
-                // the different endpoints URIs or the token validation parameters explicitly.
-                Authority = "https://oauth2.sky.blackbaud.com/"
-            };
-            
-            options.Scope.Clear();
-            
-            app.UseOpenIdConnectAuthentication(options);
-            
             app.UseMvc(RouteConfig.RegisterRoutes);
         }
 
--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,33 +1,65 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Http.Authentication;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Flurl;
+using Flurl.Http;
 
 namespace Blackbaud.AuthCodeFlowTutorial.Controllers {
     public class AuthenticationController : Controller {
         
         [HttpGet("~/auth/authenticated")]
-        public ActionResult Authenticated() {
-            return Json(new { 
-                authenticated = false
-            });
+        public ActionResult Authenticated() 
+        {
+            return Json(new { authenticated = false });
         }
         
-        [HttpGet("~/auth/login")]
-        public ActionResult LogIn() {
-            // Instruct the OIDC client middleware to redirect the user agent to the identity provider.
-            // Note: the authenticationType parameter must match the value configured in Startup.cs
-            return Challenge(new AuthenticationProperties { 
-                RedirectUri = "https://localhost:5000/"
-            }, OpenIdConnectDefaults.AuthenticationScheme);
+        [HttpGet("~/auth/callback")]
+        public ActionResult Callback() {
+            
+            var responseString = "https://oauth2.sky.blackbaud.com/token"
+                .WithHeader("Authorization", "Basic " + Base64Encode("5079f009-4b13-4699-945c-3b584b265694" + ":" + "pPS8SS0dliufKJISqlgh3Cu7QTUCqdJfVUK95qiIZiU="))
+                .WithHeader("Accept", "application/x-www-form-urlencoded")
+                .PostUrlEncodedAsync(new { 
+                    grant_type = "authorization_code",
+                    code = Request.Query["code"],
+                    redirect_uri = "https://localhost:5000/auth/callback"
+                 })
+                .ReceiveString();
+            
+            string result = responseString.Result;
+            
+            return Content("Hi there! " + result);
         }
-
-        [HttpGet("~/signout"), HttpPost("~/signout")]
-        public ActionResult SignOut() {
-            // Instruct the cookies middleware to delete the local cookie created when the user agent
-            // is redirected from the identity provider after a successful authorization flow and
-            // to redirect the user agent to the identity provider to sign out.
-            return SignOut("ClientCookie", OpenIdConnectDefaults.AuthenticationScheme);
+        
+        public static string Base64Encode(string plainText) {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        
+        // public static async Task<string> GetToken() {
+        //     using (var client = new HttpClient()) 
+        //     {
+        //         var data = "";
+        //         var values = new StringContent(data);
+        //         var response = await client.PostAsync("https://oauth2.sky.blackbaud.com/token", values);
+        //         var responseString = await response.Content.ReadAsStringAsync();
+        //         return responseString.ToString();
+        //     }
+        // }
+        
+        [HttpGet("~/auth/login")]
+        public ActionResult Login() 
+        {
+            return Redirect("https://oauth2.sky.blackbaud.com/authorization?" +
+                "client_id=" + "5079f009-4b13-4699-945c-3b584b265694" +
+                "&response_type=code" + 
+                "&redirect_uri=" + "https://localhost:5000/auth/callback"
+            );
         }
     }
 }
