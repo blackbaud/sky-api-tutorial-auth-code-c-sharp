788b4c04760b1f2f521e4373da1b41df313417af
Val Kuppuswamy <Val.Kuppuswamy@blackbaud.me>
2021-04-02 13:15:03 -0400

    Updated the packages.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Blackbaud.AuthCodeFlowTutorial
@@ -7,13 +8,12 @@ namespace Blackbaud.AuthCodeFlowTutorial
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-            host.Run();
-        }
     }
 }
